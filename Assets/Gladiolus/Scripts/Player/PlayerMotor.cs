d9e98560367505293141ff1ea4363fa1aaee4edc
Roland M. Quiros <afictitiousforce@gmail.com>
2016-03-27 12:14:08 -0700

    Mostly stable PlayerMotor

--- Assets/Gladiolus/Scripts/Player/PlayerMotor.cs
+++ Assets/Gladiolus/Scripts/Player/PlayerMotor.cs
@@ -21,29 +21,24 @@ public class PlayerMotor : MonoBehaviour {
     private Vector3 _forward;
     private Vector3 _right;
 
-    public bool _isJumping = false;
-    public bool _isGrounded = false;
-
-    public float _platformSlope = 0f;
-    public Vector3 _platformOffset;
-    public Vector3 _platformNormal;
-    public Vector3 _downhill;
-
-    public Vector3 _ceilingNormal;
-    public float _ceilingSlope = 0f;
-
-    public Vector3 _runVelocity;
-    public Vector3 _jumpVelocity;
-    public Vector3 _slideVelocity;
-    public Vector3 _gravityVelocity;
-
     public Vector3 _velocity;
     public Vector3 _offset;
 
+    public Vector3 _jumpingVelocity;
+    public Vector3 _gravityVelocity;
+
+    public Vector3 _hitNormal;
+    public Vector3 _floorNormal;
+    public Vector3 _ceilingNormal;
+
+    public Vector3 _downhill;
+
+    public float _floorAngle;
+    public float _ceilingAngle;
+
     public bool DebugIsGrounded;
-    public bool DebugCollidedSides;
-    public bool DebugCollidedBottom;
-    public bool DebugHit;
+    public CollisionFlags DebugCollisionFlags;
+    
 
 	void Awake() {
         _characterController = GetComponent<CharacterController>();
@@ -57,99 +52,85 @@ public class PlayerMotor : MonoBehaviour {
 
         Move(horizontal, vertical, jump);
     }
-    
+
     void OnControllerColliderHit(ControllerColliderHit hit) {
         bool isPlatform = (PlatformLayers.value & (1 << hit.gameObject.layer)) != 0;
         bool hitBelow = (_characterController.collisionFlags & CollisionFlags.Below) != 0;
         bool hitAbove = (_characterController.collisionFlags & CollisionFlags.Above) != 0;
 
-        if (isPlatform && hitBelow) {
-            DebugHit = true;
-            _platformNormal = hit.normal;
-        } else if (hitAbove) {
-            _ceilingNormal = hit.normal;
+        if (isPlatform) {
+            if (hitBelow) {
+                _floorNormal = hit.normal;
+                _downhill = Vector3.Cross(_floorNormal, Vector3.Cross(_floorNormal, transform.up)).normalized;
+            } else if (hitAbove) {
+                _ceilingNormal = hit.normal;
+            }
+
+            _hitNormal = hit.normal;
         }
     }
 
     public void Move(float horizontal, float vertical, bool jump) {
-        _offset = Vector3.zero;
+        // These two vectors are the only things that aren't based on transform.up. Fix this maybe?
         _forward = new Vector3(Pivot.transform.forward.x, 0f, Pivot.transform.forward.z).normalized;
         _right = new Vector3(Pivot.right.x, 0f, Pivot.right.z).normalized;
-        
-        _runVelocity = RunSpeed * (horizontal * _right + vertical * _forward);
-        
-        DebugHit = false;
-        
+
+        Vector3 motionVelocity = RunSpeed * (_forward * vertical + _right * horizontal);
+
         if (_characterController.isGrounded) {
-            _platformNormal.Normalize();
-            _runVelocity = Vector3.ProjectOnPlane(_runVelocity, _platformNormal);
 
-            _platformSlope = Vector3.Angle(_platformNormal, transform.up);
-            _downhill = Vector3.Cross(_platformNormal, Vector3.Cross(_platformNormal, transform.up)).normalized;
-
-            // Check if platform is steep enough to slide down
-            if (_platformSlope >= SlideAngle) {
-                _slideVelocity += _downhill * Gravity; //Vector3.Dot(-Vector3.up * Gravity, _downhill);
+            // The floor is greater than a given angle, slide down it
+            _floorAngle = Vector3.Angle(_floorNormal, transform.up);
+            if (_floorAngle > SlideAngle) {
+                _gravityVelocity += Gravity * _downhill;
+            // If not sliding, allow the player to jump
+            } else if (jump) {
+                _gravityVelocity = JumpSpeed * transform.up;
+                _offset = Vector3.zero;
+            // If neither sliding nor jumping, cancel gravity entirely
             } else {
-                // Cancel sliding on shallow inclines
-                _slideVelocity = Vector3.zero;
+                _gravityVelocity = Vector3.zero;
+                _offset = -_characterController.stepOffset * transform.up; // Push controller into ground to trigger proper isGrounded behavior
             }
 
-            bool isHitAbove = (_characterController.collisionFlags & CollisionFlags.Above) != 0;
-
-            // Jump
-            if (jump && !_isJumping) {
-                _jumpVelocity = transform.up * JumpSpeed;
-                _isJumping = true;
-            } else if (_isJumping && isHitAbove) {
-                _ceilingSlope = Vector3.Angle(_ceilingNormal, -transform.up);
-                if (_ceilingSlope < CeilingBumpAngle) {
-                    // Cancel the jump velocity if the player hits their head on a ceiling
-                    //_jumpVelocity = Vector3.zero;
-                } else {
-                    // Slide along the wall
-                }
-                //_jumpVelocity = Vector3.ProjectOnPlane(_jumpVelocity, _ceilingNormal).normalized * _jumpVelocity.magnitude;
-            } else {
-                // Attach Player to platform
-                bool platformFound = _platformDetector.CheckForPlatforms(out _platformOffset);
-
-                if (platformFound) {
-                    _offset += _platformOffset;
-                }
-
-                _jumpVelocity = Vector3.zero;
-                _isJumping = false;
-
-                // Even while the player is grounded, slightly push it into the ground to trigger the collision event and properly update CharacterController.isGrounded
-                // TODO: Replace this with an internal isGrounded
-                _gravityVelocity = Gravity * -Vector3.up;
-            }
         } else {
-            _slideVelocity = Vector3.zero;
-            _gravityVelocity += Gravity * -Vector3.up;
+            if ((_characterController.collisionFlags & CollisionFlags.Above) != 0) {
+                _ceilingAngle = Vector3.Angle(_ceilingNormal, -transform.up);
+                if (_ceilingAngle < CeilingBumpAngle) {
+                    _gravityVelocity = Vector3.zero;
+                }
+            }
+
+            _gravityVelocity -= Gravity * transform.up;
+
         }
-        
-        _velocity = Time.deltaTime * (_runVelocity + _jumpVelocity + _gravityVelocity + _slideVelocity) + _offset;
-        _characterController.Move(_velocity);
+
+        _velocity = motionVelocity + _gravityVelocity;
+
+        bool isHit = _characterController.collisionFlags != 0;
+        if (isHit) {
+            _velocity = Vector3.ProjectOnPlane(_velocity, _hitNormal);
+        }
 
         DebugIsGrounded = _characterController.isGrounded;
-        DebugCollidedSides = (_characterController.collisionFlags & CollisionFlags.CollidedSides) != 0;
-        DebugCollidedBottom = (_characterController.collisionFlags & CollisionFlags.CollidedBelow) != 0;
+        DebugCollisionFlags = _characterController.collisionFlags;
+
+        _characterController.Move(_velocity * Time.deltaTime + _offset);
     }
 
     void OnDrawGizmos() {
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawLine(transform.position, transform.position + _forward);
-        Gizmos.DrawLine(transform.position, transform.position + _right);
+        Vector3 feet = transform.position - (_characterController.height / 2f) * Vector3.up;
+        Vector3 head = transform.position + (_characterController.height / 2f) * Vector3.up;
 
         Gizmos.color = Color.cyan;
-        Gizmos.DrawLine(transform.position, transform.position + _platformNormal);
+        Gizmos.DrawLine(feet, feet + _floorNormal);
+        Gizmos.DrawLine(head, head + _ceilingNormal);
+        Gizmos.DrawLine(transform.position, transform.position + _hitNormal);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + _velocity);
 
         Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position, transform.position + _downhill);
-
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(transform.position, transform.position + _ceilingNormal);
+        Gizmos.DrawLine(feet, feet + _downhill);
     }
 }
